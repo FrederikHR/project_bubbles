e6b6a9de49cecc8b0b5fdb20ef8d8aa579dcf5f7
Frederik Rosenvinge <frederik.rosenvinge@gmail.com>
2025-01-25 20:28:09 +0100

    feat(BUB-19): inherit from obstacle class

--- scenes/pencil/Pencil.cs
+++ scenes/pencil/Pencil.cs
@@ -1,28 +1,12 @@
 using Godot;
 using System;
 
-public partial class Pencil : CharacterBody2D
+public partial class Pencil : Obstacle
 {
-    [Export]
-    private string Direction;
-
-    [Export]
-    private float WaitTime;
-
-    [Export]
-    private bool Launch = false;
-
-    private int speed = 10;
-    private Timer timer;
-
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        //spikes = GetNode<Sprite2D>("./Spikes");
-        timer = GetNode<Timer>("./LaunchTimer");
-        timer.WaitTime = WaitTime;
-        timer.Start();
-        GD.Print("WaitTime: ", timer.WaitTime);
+        base._Ready();
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -31,28 +15,32 @@ public partial class Pencil : CharacterBody2D
         Vector2 velocity = Velocity;
         if (Launch)
         {
-            if (Direction == "RIGHT_UP_DIAGONAL")
+            switch (direction)
             {
-                velocity.X += speed;
-                velocity.Y -= speed;
+                case Direction.RIGHT_UP_DIAGONAL:
+
+                    velocity.X += speed;
+                    velocity.Y -= speed;
+                    break;
+
+                case Direction.RIGHT_DOWN_DIAGONAL:
+
+                    velocity.X -= speed;
+                    velocity.Y += speed;
+                    break;
+
+                case Direction.LEFT_UP_DIAGONAL:
+
+                    velocity.X -= speed;
+                    velocity.Y -= speed;
+                    break;
+
+                case Direction.LEFT_DOWN_DIAGONAL:
+
+                    velocity.X += speed;
+                    velocity.Y += speed;
+                    break;
             }
-            else if (Direction == "RIGHT_DOWN_DIAGONAL")
-            {
-                velocity.X -= speed;
-                velocity.Y += speed;
-            }
-            else if (Direction == "LEFT_UP_DIAGONAL")
-            {
-                velocity.X -= speed;
-                velocity.Y -= speed;
-                
-            }
-            else if (Direction == "LEFT_DOWN_DIAGONAL")
-            {
-                velocity.X += speed;
-                velocity.Y += speed;
-            }
-            ;
             Velocity = velocity;
 
             MoveAndSlide();
